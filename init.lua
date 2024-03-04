f64d8d91a38b7bebe2b2812c01be3cd060a1af17
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-03-04 11:59:24 +0100

    Add link to video, enable clangd and pyright

--- init.lua
+++ init.lua
@@ -20,6 +20,8 @@
 =====================================================================
 =====================================================================
 
+Video tutorial: https://youtu.be/m8C0Cq9Uv9o?si=CAewnFmQpUdG16oJ
+
 What is Kickstart?
 
   Kickstart.nvim is *not* a distribution.
@@ -531,9 +533,9 @@ require('lazy').setup {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
