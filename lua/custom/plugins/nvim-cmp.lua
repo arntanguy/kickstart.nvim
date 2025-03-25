55a650c00692fda7d22f985e8bd33637cd27f131
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-03-25 15:55:22 +0100

    [luasnip] configure snippets including mc-rtc

--- lua/custom/plugins/nvim-cmp.lua
+++ lua/custom/plugins/nvim-cmp.lua
@@ -3,18 +3,7 @@ return { -- Autocompletion
   event = 'InsertEnter',
   dependencies = {
     -- Snippet Engine & its associated nvim-cmp source
-    {
-      'L3MON4D3/LuaSnip',
-      build = (function()
-        -- Build Step is needed for regex support in snippets
-        -- This step is not supported in many windows environments
-        -- Remove the below condition to re-enable on windows
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-    },
+    'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
 
     -- Adds other completion capabilities.
@@ -37,7 +26,6 @@ return { -- Autocompletion
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    luasnip.config.setup {}
 
     -- Controls how the completion items appear
     -- lspkind formatter (icons similar to vscode)
