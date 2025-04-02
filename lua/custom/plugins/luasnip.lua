168b9c801742caeb01c53c727fdc95e63327bf92
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-02 18:51:29 +0200

    Upgrade to nvim 0.11: remove luasnip

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -1,16 +1,16 @@
 return {
-  "L3MON4D3/LuaSnip",
-  dependencies = {
-     "arntanguy/mc-rtc-snippets",
-     "rafamadriz/friendly-snippets"
-  },
-  config = function()
-    local luasnip = require("luasnip")
-    -- load snippets from path/of/your/nvim/config/my-cool-snippets
-    -- local snippet_path = vim.fn.stdpath("data") .. "/lazy/"
-    -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { snippet_path .. "mc-rtc-snippets" } })
-    -- print('loading from ' .. snippet_path .. 'mc-rtc-snippets')
-    require("luasnip.loaders.from_vscode").lazy_load({})
-    luasnip.setup({})
-  end
+  -- "L3MON4D3/LuaSnip",
+  -- dependencies = {
+  --    "arntanguy/mc-rtc-snippets",
+  --    "rafamadriz/friendly-snippets"
+  -- },
+  -- config = function()
+  --   local luasnip = require("luasnip")
+  --   -- load snippets from path/of/your/nvim/config/my-cool-snippets
+  --   -- local snippet_path = vim.fn.stdpath("data") .. "/lazy/"
+  --   -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { snippet_path .. "mc-rtc-snippets" } })
+  --   -- print('loading from ' .. snippet_path .. 'mc-rtc-snippets')
+  --   require("luasnip.loaders.from_vscode").lazy_load({})
+  --   luasnip.setup({})
+  -- end
 }
