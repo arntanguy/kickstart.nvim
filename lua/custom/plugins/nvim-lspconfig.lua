654f5f3a3aa90b71637b1ae5e26e76dbdb7537ed
Arnaud TANGUY <arn.tanguy@gmail.com>
2025-04-15 10:45:23 +0200

    use pylsp by default

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -120,7 +120,7 @@ return {
     require("mason").setup()
     require("mason-lspconfig").setup(
       {
-        ensure_installed = {"clangd", "pyright", "lua_ls"},
+        ensure_installed = {"clangd", "pylsp", "lua_ls"},
         automatic_installation = true,
         handlers = {
           pylsp = function()
